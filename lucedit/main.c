dd4a9ed17d5a6877de0708eb5ae6768308870a9f
Hadi Chokr <hadichokr@icloud.com>
2025-10-22 11:42:44 +0200

    dont annoy me

--- lucedit/main.c
+++ lucedit/main.c
@@ -1,10 +1,21 @@
 #include <ncurses.h>
 
-int main(void) {
-    initscr();            // start curses mode
-    printw("Hello, Lucedit! Press any key to exit."); 
-    refresh();            // print it on the screen
-    getch();              // wait for input
-    endwin();             // end curses mode
+int main(int argc, char ** argv)
+{
+    // init screen and sets up screen
+    initscr();
+
+    // print to screen
+    printw("Hello, Lucedit! Press any key to exit.");
+
+    // refreshes the screen
+    refresh();
+
+    // pause the screen output
+    getch();
+
+    // deallocates memory and ends ncurses
+    endwin();
+
     return 0;
 }
