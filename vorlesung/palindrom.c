c07c065cc81f4852230da6d0cdbadbcca9266535
Hadi Chokr <hadichokr@icloud.com>
2025-10-29 11:47:09 +0100

    good palindrom

--- vorlesung/palindrom.c
+++ vorlesung/palindrom.c
@@ -1,24 +1,18 @@
-# include "stdio.h"
-# include "stdbool.h"
-# include "string.h"
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
-bool is_palindrom(const char* eingabe){
-    int lenght = strlen(eingabe);
-    char first[(lenght / 2) + 1];
-    char second[lenght - (lenght / 2) + 1];
-
-    strncpy(first, eingabe, (lenght / 2));
-    first[(lenght / 2)] = '\0'; // null-terminate
-    strcpy(second, eingabe + (lenght / 2)); 
-
-    if (strcmp(second, first) == 0) {
-        return true;
-    } else {
-        return false;
+bool is_palindrom(const char *eingabe) {
+    int len = strlen(eingabe);
+    for (int i = 0; i < len / 2; i++) {
+        if (eingabe[i] != eingabe[len - i - 1]) {
+            return false;
+        }
     }
+    return true;
 }
 
-int main() {
+int main(void) {
     char eingabe[100];
     printf("Gebe ein Wort (Max: 99 Buchstaben) ein: \n");
     scanf("%99s", eingabe);
@@ -26,7 +20,7 @@ int main() {
     if (is_palindrom(eingabe)) {
         printf("%s ist ein Palindrom.\n", eingabe);
     } else {
-        printf("%s ist ein kein Palindrom.\n", eingabe);
+        printf("%s ist kein Palindrom.\n", eingabe);
     }
 
     return 0;
